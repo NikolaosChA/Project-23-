b9fcb912bbe989c55a5b9b68ab067020e91f8065
Nikos <n.ch.alex.95@gmail.com>
2021-10-11 21:12:21 +0300

    initial commit

---
+++ sixdays/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-vm1$gq31!1*%n7fn9k#v4(e*p==2j2e+fp*e8d5mfpd+=33wko'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
